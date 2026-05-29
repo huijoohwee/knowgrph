a3f28d8661fa057e073eaaf34a34755e69a1a1f0
huijoohwee <8945812+huijoohwee@users.noreply.github.com>
2026-05-29 17:08:41 +0800

    build(canvas): add tailwind blocklist to restrict invalid classes

--- canvas/tailwind.config.js
+++ canvas/tailwind.config.js
@@ -7,6 +7,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "../gympgrph/src/**/*.{js,ts,jsx,tsx}",
   ],
+  blocklist: [
+    "[-:_./\\\\]",
+    "[a-z0-9:_-]",
+    "[A-Za-z0-9:_-]",
+    "[A-Za-z0-9^:._-]",
+    "[A-Za-z0-9_.-]",
+    "[A-Za-z0-9_:-]",
+  ],
   theme: {
     container: {
       center: true,
